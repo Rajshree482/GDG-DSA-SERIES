abb6a2b0043b4f6a59ca86b080087de20f1503ff
Rajshree Balsamant <rajshreebalsamant@gmail.com>
2025-12-18 22:36:49 +0530

    selection Sort

---
+++ Set1_Day1_2/Java_DSA_series/src/SelectionSort.java
@@ -0,0 +1,31 @@
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i; //min element
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] ar = {29, 10, 14, 37, 14, 5};
+        System.out.println("Original array:");
+        printArray(ar);
+        selectionSort(ar);
+        System.out.println("Sorted array:");
+        printArray(ar);
+    }
+}
